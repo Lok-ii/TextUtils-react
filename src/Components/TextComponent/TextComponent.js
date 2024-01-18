36680cc36ea3500792c22ef06d6ccb730f02bf86
Lok-ii <lokeshkataria.work@gmail.com>
2024-01-18 13:36:17 +0530

    updated

--- src/Components/TextComponent/TextComponent.js
+++ src/Components/TextComponent/TextComponent.js
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useReducer, useRef } from "react";
 import "./textcomponent.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const TextComponent = () => {
   const textRef = useRef(null);
   const [disbled, setDisabled] = useState(true);
 
+  const notify = (type, value) => {
+    if (type === "UPPERCASE" || type === "LOWERCASE" || type === "COPY") {
+      toast.success(value, {
+        position: "top-left",
+      });
+    } else if (type === "CLEARED") {
+      toast.error(value, {
+        position: "top-left",
+      });
+    }else{
+        toast.info(value, {
+      position: "top-left",
+    });
+    }
+
+    
+  };
   const textFunc = (state, action) => {
     switch (action) {
       case "UPPERCASE":
@@ -27,18 +46,19 @@ const TextComponent = () => {
   };
 
   const [state, dispatch] = useReducer(textFunc, "");
-function checkDisabled(){
-        if(state == ""){
-            setDisabled(true);
-        }else{
-            setDisabled(false);
-        }
-      }
-  useEffect(()=>{
-      checkDisabled();
+  function checkDisabled() {
+    if (state == "") {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }
+  useEffect(() => {
+    checkDisabled();
   }, [state]);
   return (
     <div className="textComponent">
+      <ToastContainer />
       <h1 className="heading">
         TextUtis - Word Counter, Charecter Counter, Remove Extra Space
       </h1>
@@ -57,19 +77,52 @@ function checkDisabled(){
         ></textarea>
       </div>
       <div className="btnContainer">
-        <button className="upper" disabled={disbled} onClick={() => dispatch("UPPERCASE")}>
+        <button
+          className="upper"
+          disabled={disbled}
+          onClick={() => {
+            dispatch("UPPERCASE");
+            notify("UPPERCASE", "CONVERTED TO UPPERCASE!");
+          }}
+        >
           Convert to Uppercase
         </button>
-        <button className="lower" disabled={disbled} onClick={() => dispatch("LOWERCASE")}>
+        <button
+          className="lower"
+          disabled={disbled}
+          onClick={() => {
+            dispatch("LOWERCASE");
+            notify("LOWERCASE", "converted to lowercase!");
+          }}
+        >
           Convert to Lowercase
         </button>
-        <button className="text" disabled={disbled} onClick={() => dispatch("CLEAR")}>
+        <button
+          className="text"
+          disabled={disbled}
+          onClick={() => {
+            dispatch("CLEAR");
+            notify("CLEARED", "Text has been cleared!");
+          }}
+        >
           Clear Text
         </button>
-        <button className="copy" disabled={disbled} onClick={() => dispatch("COPY")}>
+        <button
+          className="copy"
+          disabled={disbled}
+          onClick={() => {
+            dispatch("COPY");
+            notify("COPY", "Successfully copied!");
+          }}
+        >
           Copy to Clipboard
         </button>
-        <button className="remove" disabled={disbled} onClick={() => dispatch("REMOVE")}>
+        <button
+          className="remove"
+          disabled={disbled}
+          onClick={() => {dispatch("REMOVE")
+        notify("REMOVE", "Removed extra spaces!")}}
+        >
           Remove Extra Spaces
         </button>
       </div>
@@ -85,8 +138,7 @@ function checkDisabled(){
         <p className="time">
           Reading Time:{" "}
           {(
-            0.008 *
-            state.split(" ").filter((e) => e.length !== 0).length
+            0.008 * state.split(" ").filter((e) => e.length !== 0).length
           ).toFixed(3)}{" "}
           Minutes
         </p>
@@ -94,7 +146,12 @@ function checkDisabled(){
 
       <div className="previewContainer">
         <h2 className="previewTitle">Preview Document</h2>
-        <textarea name="previewBox" id="previewBox" value={state} onKeyUp={()=>alert("Cannot Edit Preview Document")}></textarea>
+        <textarea
+          name="previewBox"
+          id="previewBox"
+          value={state}
+          onKeyUp={() => alert("Cannot Edit Preview Document")}
+        ></textarea>
       </div>
     </div>
   );
